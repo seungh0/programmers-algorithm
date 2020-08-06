e6dfc561c6df909d5637422479259f66a3715405
seungh0 <will.seungho@gmail.com>
2020-08-06 23:58:00 +0900

    진법 변환 2

--- 20200806/convert_to_decimal.py
+++ 20200806/convert_to_decimal.py
@@ -1,6 +1,15 @@
 import unittest
 
 
+def convert_to_decimal_2(number, base):
+    multiplier, result = 1, 0
+    while number > 0:
+        result += number % 10 * multiplier
+        multiplier *= base
+        number = number // 10
+    return result
+
+
 def convert_to_decimal(number, base):
     answer = 0
     str_num = str(number)
@@ -17,6 +26,16 @@ class TestCase(unittest.TestCase):
     def test_base_8(self):
         number, base = 111, 8
         self.assertEqual(convert_to_decimal(number, base), 73)
+
+
+class TestCase2(unittest.TestCase):
+    def test_base_2(self):
+        number, base = 1001, 2
+        self.assertEqual(convert_to_decimal_2(number, base), 9)
+
+    def test_base_8(self):
+        number, base = 111, 8
+        self.assertEqual(convert_to_decimal_2(number, base), 73)
 
 
 if __name__ == "__main__":
