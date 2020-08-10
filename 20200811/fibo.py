162e138aca466e425011aa3ec2bd15f6805e3b7b
seungh0 <will.seungho@gmail.com>
2020-08-11 00:27:01 +0900

    피보나치 제너레이터

--- 20200811/fibo.py
+++ 20200811/fibo.py
@@ -16,6 +16,13 @@ def solution(n):
     return solution(n - 2) + solution(n - 1)
 
 
+def solution_generate():
+    a, b = 0, 1
+    while True:
+        a, b = b, a + b
+        yield a
+
+
 # 1 1 2 3 5
 class TestCase(unittest.TestCase):
     def test_fibo_case_1(self):
@@ -32,7 +39,19 @@ class TestCase(unittest.TestCase):
         # when
         result = solution_1(n)
         # then
+
         self.assertEqual(result, 5)
+
+    def test_fibo_case_3(self):
+        # given
+        n = 10
+        result = 0
+        # when
+        fg = solution_generate()
+        for _ in range(n):
+            result = next(fg)
+        # then
+        self.assertEqual(result, 55)
 
 
 if __name__ == "__main__":
