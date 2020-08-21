1f98d64fd462f0e73b76b04f455d117a4e993487
seungh0 <will.seungho@gmail.com>
2020-08-21 21:35:26 +0900

    구현 - 상하좌우 책 풀이

--- 20200821/first.py
+++ 20200821/first.py
@@ -35,11 +35,34 @@ def solution(n, plan):
     return [point.x, point.y]
 
 
+def solution_book(n, plans):
+    x, y = 1, 1
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+    move_types = ['L', 'R', 'U', 'D']
+
+    for plan in plans:
+        for i in range(len(move_types)):
+            if plan == move_types[i]:
+                nx = x + dx[i]
+                ny = y + dy[i]
+        if nx < 1 or ny < 1 or nx > n or ny > n:
+            continue
+        x, y = nx, ny
+    return [x, y]
+
+
 class MyTestCase(unittest.TestCase):
     def test_solution_case_1(self):
         n = 5
         plan = ['R', 'R', 'R', 'U', 'D', 'D']
         result = solution(n, plan)
+        self.assertEqual(result, [3, 4])
+
+    def test_solution_case_2(self):
+        n = 5
+        plan = ['R', 'R', 'R', 'U', 'D', 'D']
+        result = solution_book(n, plan)
         self.assertEqual(result, [3, 4])
 
 
