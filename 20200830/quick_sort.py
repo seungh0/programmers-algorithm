0dd7c9e174f064e6e9d87978de4d4fd868b41dc9
seungh0 <will.seungho@gmail.com>
2020-08-31 00:55:46 +0900

    퀵 정렬 Solution 2

--- 20200830/quick_sort.py
+++ 20200830/quick_sort.py
@@ -21,10 +21,27 @@ def solution(arr, start, end):
     return arr
 
 
+def solution_two(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+    tail = arr[1:]
+
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+    return solution_two(left_side) + [pivot] + solution_two(right_side)
+
+
 class QuickSortTest(unittest.TestCase):
     def test_case_1(self):
         arr = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
         result = solution(arr, 0, len(arr) - 1)
+        self.assertEqual(result, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    def test_case_2(self):
+        arr = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+        result = solution_two(arr)
         self.assertEqual(result, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 
