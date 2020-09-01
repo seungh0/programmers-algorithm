0575fcf3eaf3396011d1d78af18961a4b00bdd76
seungh0 <will.seungho@gmail.com>
2020-09-02 00:19:49 +0900

    BinarySearch - Loop

--- 20200901/binary_search.py
+++ 20200901/binary_search.py
@@ -12,6 +12,18 @@ def binary_search(array, target, start, end):
     return mid
 
 
+def binary_search_2(arr, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] > target:
+            end = mid - 1
+        elif arr[mid] < target:
+            start = mid + 1
+        else:
+            return mid
+    return -1
+
+
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
@@ -21,6 +33,16 @@ class MyTestCase(unittest.TestCase):
     def test_something_1(self):
         arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
         result = binary_search(arr, 4, 0, len(arr) - 1)
+        self.assertEqual(result, -1)
+
+    def test_something_solution_1(self):
+        arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+        result = binary_search_2(arr, 5, 0, len(arr) - 1)
+        self.assertEqual(result, 2)
+
+    def test_something_solution_2(self):
+        arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+        result = binary_search_2(arr, 4, 0, len(arr) - 1)
         self.assertEqual(result, -1)
 
 
