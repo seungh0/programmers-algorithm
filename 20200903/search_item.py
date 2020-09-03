fde10fedda0b1b8b869fab9c3f126b40697126df
seungh0 <will.seungho@gmail.com>
2020-09-03 19:32:51 +0900

    부품찾기 - 이진 탐색

--- 20200903/search_item.py
+++ 20200903/search_item.py
@@ -16,11 +16,40 @@ def solution(item, search):
     return result
 
 
+def binary_search(items, item, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if items[mid] == item:
+        return mid
+    elif items[mid] > item:
+        return binary_search(items, item, start, mid - 1)
+    else:
+        return binary_search(items, item, mid + 1, end)
+
+
+def solution_2(item, search):
+    result = []
+    item.sort()
+    for i in search:
+        if binary_search(item, i, 0, len(item) - 1):
+            result.append("yes")
+        else:
+            result.append('no')
+    return result
+
+
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         item = [8, 3, 7, 9, 2]
         search = [5, 7, 9]
         result = solution(item, search)
+        self.assertEqual(result, ['no', 'yes', 'yes'])
+
+    def test_something(self):
+        item = [8, 3, 7, 9, 2]
+        search = [5, 7, 9]
+        result = solution_2(item, search)
         self.assertEqual(result, ['no', 'yes', 'yes'])
 
 
