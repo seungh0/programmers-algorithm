40af50804034b50f6cb78c8c926bee16f8d9cb41
seungh0 <will.seungho@gmail.com>
2020-09-13 22:05:08 +0900

    모험가 길드

---
+++ 20200913/adventure_group.py
@@ -0,0 +1,36 @@
+import unittest
+from collections import deque
+
+
+def solution(people):
+    people.sort()
+    queue = deque(people)
+    result = 0
+    count = 0
+    for i in queue:
+        count += 1
+        if count >= i:
+            result += 1
+            count = 0
+    return result
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        man = [2, 3, 1, 2, 2]
+        result = solution(man)
+        self.assertEqual(result, 2)
+
+    def test_something2(self):
+        man = [5, 3, 1, 2, 2]
+        result = solution(man)
+        self.assertEqual(result, 2)
+
+    def test_something3(self):
+        man = [6, 1, 1, 1, 1]
+        result = solution(man)
+        self.assertEqual(result, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
