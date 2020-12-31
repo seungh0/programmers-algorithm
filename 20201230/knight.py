58016acda65189335c637e1359742ccf47a9a074
seungh0 <will.seungho@gmail.com>
2020-12-31 16:22:19 +0900

    feat(ch4): 게임 개발

--- 20201230/knight.py
+++ 20201230/knight.py
@@ -30,7 +30,7 @@ def solution(pos):
 class TestCase(unittest.TestCase):
     def test_case_1(self):
         result = solution('a1')
-        self.assertEqual트(result, 2)
+        self.assertEqual(result, 2)
 
     def test_case_2(self):
         result = solution('c2')
